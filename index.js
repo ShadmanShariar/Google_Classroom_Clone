bfdc70938c09b8fcc67a1f109023a7ee2adb5f86
Shadman Shariar <shadmanshariar007@gmail.com>
2023-06-01 23:48:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function signup(){
+
+alert("Sign up successful");
+window.location.href = "index.html";
+
+}
+function join(){
+
+    prompt("Enter your class code");
+    
+    }
+function home(){
+
+window.location.href ="home.html";
+
+
+
+}
